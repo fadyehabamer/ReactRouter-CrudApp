0c83c1f93fd4d000103510120dad7afef9ae3ac5
fadyehabamer <fadyamer45@gmail.com>
2026-09-24 16:23:32 +0300

    fix: make the salary filter a reversible view
    "Filter above 2500" replaced the displayed list with the filtered copy,
    with no way back, and delete/edit then used positions in that filtered
    copy. Toggle a view filter instead and keep each row's real index.

--- src/ListStd.js
+++ src/ListStd.js
@@ -3,25 +3,19 @@ import { Link } from "react-router-dom";
 import "./App.css"
 export default class ListStd extends React.Component {
 
+  // the filter is a view toggle; the full list stays in App
   state = {
-    StudentsList: this.props.Students,
+    onlyAbove2500: false,
   };
-
-  componentDidUpdate(prevProps) {
-    if (prevProps.Students !== this.props.Students) {
-      this.setState({ StudentsList: this.props.Students });
-    }
-  }
 
   SortBySalary = () => {
     this.props.onSort();
   };
 
   FilterSalaryAbove = () => {
-    let newList = this.state.StudentsList.filter((std) => std.salary > 2500);
-    this.setState({
-      StudentsList: newList,
-    });
+    this.setState((prev) => ({
+      onlyAbove2500: !prev.onlyAbove2500,
+    }));
   };
 
 
@@ -50,7 +44,7 @@ export default class ListStd extends React.Component {
 
           <input
             type="button"
-            value="Filter above 2500"
+            value={this.state.onlyAbove2500 ? "Show all" : "Filter above 2500"}
             onClick={() => this.FilterSalaryAbove()}
           />
         </div>
@@ -58,7 +52,9 @@ export default class ListStd extends React.Component {
         <br />
 
         <ul>
-          {this.state.StudentsList.map((std, index) => {
+          {this.props.Students.map((std, index) => {
+            // keep the real index so delete/edit target the right employee
+            if (this.state.onlyAbove2500 && !(std.salary > 2500)) return null;
             return (
               <li key={index}>
                 <span>
